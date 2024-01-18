5c31888159f3e5a8c07f5bf8077ba2aab52b6382
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2024-01-18 19:47:47 +0530

    some changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,9 +8,9 @@ const Login = () => {
                 <form action="" className='myForm'>
                     <h1>Login</h1>
                     <label htmlFor="username">Username</label><br />
-                    <input type="text" name="username" id="username" /><br />
+                    <input type="text" name="username" id="username" placeholder='Enter Username'/><br />
                     <label htmlFor="password">Password</label><br />
-                    <input type="text" name="password" id="password" /><br /><br />
+                    <input type="text" name="password" id="password" placeholder="Enter Pasword"/><br /><br />
                     <input type="button" value="Login" />
                 </form>
             </div>
