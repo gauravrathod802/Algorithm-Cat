a0aaab09c26246bcbe16b42187792b890530a843
gauravrathod802 <98140248+gauravrathod802@users.noreply.github.com>
2024-01-20 20:19:57 +0530

    added some styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,8 +10,8 @@ const Login = () => {
                     <label htmlFor="username">Username</label><br />
                     <input type="text" name="username" id="username" placeholder='Enter Username'/><br />
                     <label htmlFor="password">Password</label><br />
-                    <input type="text" name="password" id="password" placeholder="Enter Pasword"/><br /><br />
-                    <input type="button" value="Login" />
+                    <input type="password" name="password" id="password" placeholder="Enter Pasword"/><br /><br />
+                    <input type="submit" value="Login" className='loginBtn'/>
                 </form>
             </div>
         </div>
